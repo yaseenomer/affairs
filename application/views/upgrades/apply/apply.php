a8ad59bfa3bf82ad2654a2b76a4a0ea1b5aa30ac
softsust@gmail.com <softsust@gmail.com>
2018-05-09 15:02:04 +0300

    update apply view

--- application/views/upgrades/apply/apply.php
+++ application/views/upgrades/apply/apply.php
@@ -4,26 +4,35 @@
     <div class="row">
         <div class="col-md-3 col-sm-3 col-xs-3">
             <ul class="nav nav-tabs tabs-left">
-                <li class="active"><a href="#home" data-toggle="tab">
-                        <i class="fa fa-user-o"> </i>البيانات الشخصية
+                <li class="active"><a href="#coll" data-toggle="tab">
+                        <i class="fa fa-user-o"> </i> معلومات عن الكلية
                     </a>
                 </li>
 
                 <li class=""><a href="#profile" data-toggle="tab">
                         <i class="fa fa-graduation-cap"></i>
-                        البيانات الاكاديمية
+                         معلومات عن مقدم الطلب
+                    </a>
+                </li>
+
+                <li class=""><a href="#holiday" data-toggle="tab">
+                        <i class="fa fa-graduation-cap"></i>
+                           الإجازات
                     </a>
                 </li>
             </ul>
         </div>
         <div class="col-md-9 col-sm-9 col-xs-9">
             <div class="tab-content">
-                <div class="tab-pane fade active in" id="home">
+                <div class="tab-pane fade active in" id="coll">
                     الاساسية
-
                 </div>
 
                 <div class="tab-pane fade" id="profile">
+                     الاكاديمية
+                </div>
+
+                <div class="tab-pane fade" id="holiday">
                      الاكاديمية
                 </div>
             </div>
