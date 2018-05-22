0b55f0111db90bd5d657440143b5c078b1b697e1
elfrashah <sarah22nugdallah@hotmail.com>
2018-05-22 13:32:41 +0200

    delete user folder

--- yassinnn/madule users/assets/js/ajax.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $('#example, #elevel5_id').DataTable();
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    });
-
-
-});
--- yassinnn/madule users/view/auth/fail.php
+++
@@ -1,6 +0,0 @@
-
-
-<div class="col-md-6 col-md-offset-3 o_database_list">
-    <div class="text-center">
-<p>Fail in login . <a href="<?php echo base_url(); ?>Logincont/index">Press here to try Agin.</a></p>
-    </div>
